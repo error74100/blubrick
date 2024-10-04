6b9aa21323700c63979832e6010bbb3fbced9ebf
error7410 <error7410@gmail.com>
2024-10-04 14:43:41 +0900

    생활안내, 주변환경 글자효과 추가

--- src/app/environment/page.tsx
+++ src/app/environment/page.tsx
@@ -26,7 +26,7 @@ export default function Page() {
               <ul className={style.group}>
                 <li>
                   <div className={style.img_wrap}>
-                    <p className={style.img}>
+                    <p className={style.img} data-aos="fade-right">
                       <img
                         src="/img/temp_environment_img_01.jpg"
                         alt="주변환경 이미지"
@@ -35,11 +35,11 @@ export default function Page() {
                   </div>
 
                   <div className={style.cont_wrap}>
-                    <h2>주변환경</h2>
-                    <p className={style.indent}>
+                    <h2 data-aos="fade-left">주변환경</h2>
+                    <p className={style.indent} data-aos="fade-left">
                       주변에도 소음을 유발하는 영업장이 없어서 조용합니다.
                     </p>
-                    <p className={style.indent}>
+                    <p className={style.indent} data-aos="fade-left">
                       1층에 편의점과 커피 전문점 입점해있으며 1분거리 수협이
                       있어서 생활하기 편리한 곳입니다.
                     </p>
@@ -48,7 +48,7 @@ export default function Page() {
 
                 <li>
                   <div className={style.img_wrap}>
-                    <p className={style.img}>
+                    <p className={style.img} data-aos="fade-left">
                       <img
                         src="/img/temp_environment_img_01.jpg"
                         alt="교통환경 이미지"
@@ -57,22 +57,33 @@ export default function Page() {
                   </div>
 
                   <div className={style.cont_wrap}>
-                    <h2>교통환경</h2>
-                    <p className={style.indent}>
+                    <h2 data-aos="fade-right">교통환경</h2>
+                    <p className={style.indent} data-aos="fade-right">
                       노량진역 1,9호선에서 5분거리.
                     </p>
-                    <p className={style.indent}>삼익 수협 마트바로 뒤.</p>
+                    <p className={style.indent} data-aos="fade-right">
+                      삼익 수협 마트바로 뒤.
+                    </p>
 
                     <br />
 
-                    <p className={`${style.indent} ${style.type2}`}>
+                    <p
+                      className={`${style.indent} ${style.type2}`}
+                      data-aos="fade-right"
+                    >
                       <b>＊버스노선</b>
                     </p>
-                    <p className={`${style.indent} ${style.type2}`}>
+                    <p
+                      className={`${style.indent} ${style.type2}`}
+                      data-aos="fade-right"
+                    >
                       <span className="badge bg-primary">일반</span> 100, 200,
                       300
                     </p>
-                    <p className={`${style.indent} ${style.type2}`}>
+                    <p
+                      className={`${style.indent} ${style.type2}`}
+                      data-aos="fade-right"
+                    >
                       <span className="badge bg-success">좌석</span> 1000, 2022
                     </p>
                   </div>
--- src/app/information/page.tsx
+++ src/app/information/page.tsx
@@ -26,7 +26,7 @@ export default function Page() {
               <ul className={style.group}>
                 <li>
                   <div className={style.img_wrap}>
-                    <p className={style.img}>
+                    <p className={style.img} data-aos="fade-right">
                       <img
                         src="/img/temp_information_img_01.jpg"
                         alt="풀옵션 원룸텔 이미지"
@@ -35,15 +35,15 @@ export default function Page() {
                   </div>
 
                   <div className={style.cont_wrap}>
-                    <h2>풀옵션 원룸텔</h2>
-                    <p className={style.indent}>
+                    <h2 data-aos="fade-left">풀옵션 원룸텔</h2>
+                    <p className={style.indent} data-aos="fade-left">
                       개인 냉장고, 옷장, 책장 포함이며 벽돌시공으로 소음이
                       적습니다.
                     </p>
-                    <p className={style.indent}>
+                    <p className={style.indent} data-aos="fade-left">
                       주변에도 소음을 유발하는 영업장이 없어서 조용합니다.
                     </p>
-                    <p className={style.indent}>
+                    <p className={style.indent} data-aos="fade-left">
                       방에 따라 베란다, 개인 화장실, 샤워실, 개인 에어컨
                       포함입니다.
                     </p>
@@ -52,7 +52,7 @@ export default function Page() {
 
                 <li>
                   <div className={style.img_wrap}>
-                    <p className={style.img}>
+                    <p className={style.img} data-aos="fade-left">
                       <img
                         src="/img/temp_information_img_01.jpg"
                         alt="인터넷 환경 이미지"
@@ -61,8 +61,8 @@ export default function Page() {
                   </div>
 
                   <div className={style.cont_wrap}>
-                    <h2>인터넷 환경</h2>
-                    <p className={style.indent}>
+                    <h2 data-aos="fade-right">인터넷 환경</h2>
+                    <p className={style.indent} data-aos="fade-right">
                       초고속 wifi 설치되어 있습니다.
                     </p>
                   </div>
@@ -70,7 +70,7 @@ export default function Page() {
 
                 <li>
                   <div className={style.img_wrap}>
-                    <p className={style.img}>
+                    <p className={style.img} data-aos="fade-right">
                       <img
                         src="/img/temp_information_img_01.jpg"
                         alt="주방시설 및 세탁 이미지"
@@ -79,12 +79,12 @@ export default function Page() {
                   </div>
 
                   <div className={style.cont_wrap}>
-                    <h2>주방시설 및 세탁</h2>
-                    <p className={style.indent}>
+                    <h2 data-aos="fade-left">주방시설 및 세탁</h2>
+                    <p className={style.indent} data-aos="fade-left">
                       밥솥, 정수기, 인덕션, 전자레인지, 냄비, 그릇, 수저, 라면,
                       쌀 등이 있습니다.
                     </p>
-                    <p className={style.indent}>
+                    <p className={style.indent} data-aos="fade-left">
                       세탁기, 세탁세제 등이 있습니다.
                     </p>
                   </div>
@@ -92,7 +92,7 @@ export default function Page() {
 
                 <li>
                   <div className={style.img_wrap}>
-                    <p className={style.img}>
+                    <p className={style.img} data-aos="fade-left">
                       <img
                         src="/img/temp_information_img_01.jpg"
                         alt="소방시설 이미지"
@@ -101,8 +101,8 @@ export default function Page() {
                   </div>
 
                   <div className={style.cont_wrap}>
-                    <h2>소방시설</h2>
-                    <p className={style.indent}>
+                    <h2 data-aos="fade-right">소방시설</h2>
+                    <p className={style.indent} data-aos="fade-right">
                       자동 화재감지시스템 및 스프링클러가 장착되어 있습니다.
                     </p>
                   </div>
